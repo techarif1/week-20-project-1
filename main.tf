c658e82cb0b2b2eaf54f943859938e9439aeb76a
mendez-m85 <74025449+mendez-m85@users.noreply.github.com>
2021-07-16 12:23:49 -0500

    Update main.tf
    Changed KP name.

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "compute" {
   public_subnet  = module.networking.public_subnet
   elb            = module.loadbalancing.elb
   alb_tg         = module.loadbalancing.alb_tg
-  key_name       = "MyPrivateKey"
+  key_name       = "my-py-kp"
 }
 
 module "loadbalancing" {
